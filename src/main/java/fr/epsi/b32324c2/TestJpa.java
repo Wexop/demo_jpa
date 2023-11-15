b0ac903547f939d661bcdf3431b82e44e356e12c
Wexop <mattheurtin@gmail.com>
2023-11-15 11:59:11 +0100

    select titre

--- src/main/java/fr/epsi/b32324c2/TestJpa.java
+++ src/main/java/fr/epsi/b32324c2/TestJpa.java
@@ -11,16 +11,11 @@ public class TestJpa {
         EntityManager em = emf.createEntityManager();
 
 
+        TypedQuery<Livre> query2 = em.createQuery("select l from Livre l where l.titre ='ELMER'", Livre.class);
+        Livre h2 = query2.getResultList().get(0);
 
-        EntityTransaction et = em.getTransaction();
-        et.begin();
-        em.merge(new Livre(5, "Du plaisir dans la cuisine", "Jean-Pierre Coffe"));
-        et.commit(); // ou et.rollback()
-        
-
-        Livre livre = em.find(Livre.class, 5);
-        if(livre != null) {
-            System.out.println(livre.toString());
+        if(h2 != null) {
+            System.out.println(h2.toString());
         }
 
 
