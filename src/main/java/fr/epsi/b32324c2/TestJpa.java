ac1000b1626a2e81034443e46a53b03270394296
Potowai <alexis.thomas.ines@gmail.com>
2023-11-15 11:57:42 +0100

    last fix

--- src/main/java/fr/epsi/b32324c2/TestJpa.java
+++ src/main/java/fr/epsi/b32324c2/TestJpa.java
@@ -5,13 +5,36 @@ import javax.persistence.*;
 public class TestJpa {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-a1");
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManagerFactory emf = null;
+        EntityManager entityManager = null;
 
-        System.out.println(entityManager.isOpen()); // se connecte à la bdd
+        try {
+            // Create EntityManagerFactory
+            emf = Persistence.createEntityManagerFactory("pu-a1");
 
-        entityManager.close();
-        emf.close();
+            // Create EntityManager
+            entityManager = emf.createEntityManager();
 
+            System.out.println(entityManager.isOpen()); // Connect to the database
+
+            // Perform database operations within a transaction if needed
+            // Example: entityManager.getTransaction().begin();
+            // ...
+
+            // Commit the transaction if started
+            // Example: entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close EntityManager
+            if (entityManager != null && entityManager.isOpen()) {
+                entityManager.close();
+            }
+
+            // Close EntityManagerFactory
+            if (emf != null && emf.isOpen()) {
+                emf.close();
+            }
+        }
     }
 }
