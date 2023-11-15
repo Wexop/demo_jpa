6a770043663dfeac4e71088dd0968ca6ce71a66b
Wexop <mattheurtin@gmail.com>
2023-11-15 10:41:48 +0100

    tp1

--- src/main/java/fr/epsi/b32324c2/TestJpa.java
+++ src/main/java/fr/epsi/b32324c2/TestJpa.java
@@ -1,2 +1,17 @@
-package fr.epsi.b32324c2;public class TestJpa {
+package fr.epsi.b32324c2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FOURNISSEUR")
+public class TestJpa {
+
+    @Id
+    private int ID;
+
+    @Column(name = "NOM")
+    private String nom;
 }
